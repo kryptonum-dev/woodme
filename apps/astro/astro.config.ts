7e1d83e4d356e18d74fa0c331fc059507de16889
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2024-12-30 11:57:07 +0100

    fix: update vercel adapter

--- apps/astro/astro.config.ts
+++ apps/astro/astro.config.ts
@@ -1,34 +1,34 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import sitemap from "@astrojs/sitemap";
-import { DOMAIN } from "./src/global/constants";
-import { isPreviewDeployment } from "./src/utils/is-preview-deployment";
-import redirects from "./redirects";
+import { defineConfig } from 'astro/config'
+import vercel from '@astrojs/vercel/serverless'
+import sitemap from '@astrojs/sitemap'
+import { DOMAIN } from './src/global/constants'
+import { isPreviewDeployment } from './src/utils/is-preview-deployment'
+import redirects from './redirects'
 
 export default defineConfig({
   site: DOMAIN,
-  integrations: [
-    sitemap(),
-  ],
+  integrations: [sitemap()],
   image: {
-    remotePatterns: [{
-      protocol: "https",
-      hostname: "cdn.sanity.io"
-    }],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
   },
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          api: "modern",
-        }
-      }
-    }
+          api: 'modern',
+        },
+      },
+    },
   },
   prefetch: {
-    prefetchAll: true
+    prefetchAll: true,
   },
   redirects: redirects,
-  output: isPreviewDeployment ? "server" : 'hybrid',
+  output: isPreviewDeployment ? 'server' : 'hybrid',
   adapter: vercel(),
-});
+})
