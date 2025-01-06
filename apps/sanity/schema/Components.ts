c1e17f60144a02ebaad18b85d94944e091395591
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2025-01-06 09:07:00 +0100

    feat: create ExpandingImages Section

--- apps/sanity/schema/Components.ts
+++ apps/sanity/schema/Components.ts
@@ -3,12 +3,13 @@ import BlogReference from './components/BlogReference';
 import CircleSwiper from './components/CircleSwiper';
 import SplitContentSection from './components/SplitContentSection';
 import CtaSection from './components/CtaSection';
+import ExpandingImages from './components/ExpandingImages';
 
 export default defineType({
   name: 'components',
   type: 'array',
   title: 'Components',
-  of: [CircleSwiper, SplitContentSection, BlogReference, CtaSection],
+  of: [CircleSwiper, SplitContentSection, BlogReference, CtaSection, ExpandingImages],
   options: {
     insertMenu: {
       filter: true,
