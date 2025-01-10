8f72e582fec714804d44959639b96d753e128cf9
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2025-01-10 08:07:07 +0100

    feat: create TextListWithImage Section

--- apps/sanity/schema/Components.ts
+++ apps/sanity/schema/Components.ts
@@ -14,7 +14,7 @@ import ListPointsMiddlePhoto from './components/ListPointsMiddlePhoto';
 import MiddleImageCtas from './components/MiddleImageCtas';
 import SingleHeadingSection from './components/SingleHeadingSection';
 import ImageHeadingColumns from './components/ImageHeadingColumns';
-
+import TextListWithImage from './components/TextListWithImage';
 export default defineType({
   name: 'components',
   type: 'array',
@@ -35,6 +35,7 @@ export default defineType({
     SingleHeadingSection,
     MiddleImageCtas,
     ImageHeadingColumns,
+    TextListWithImage,
   ],
   options: {
     insertMenu: {
