7a10c09ec0c64d2b14eb8fc4ca9f8b5ecc5ecec5
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2025-01-30 08:48:43 +0100

    fix: change sanityFetch fn

--- apps/astro/src/utils/sanity.fetch.ts
+++ apps/astro/src/utils/sanity.fetch.ts
@@ -7,15 +7,13 @@ if (isPreviewDeployment && !SANITY_API_TOKEN) {
   console.warn('\x1b[33m%s\x1b[0m', 'The `SANITY_API_TOKEN` environment variable is required.');
 }
 
-console.log(isPreviewDeployment);
-
 export const client = createClient({
   projectId: '7fuzao96',
   dataset: 'production',
   apiVersion: '2024-12-30',
-  useCdn: false,
-  perspective: 'previewDrafts',
-  // ...(isPreviewDeployment && { token: SANITY_API_TOKEN }),
+  useCdn: !isPreviewDeployment,
+  perspective: isPreviewDeployment ? 'previewDrafts' : 'published',
+  token: SANITY_API_TOKEN,
 });
 
 export default async function sanityFetch<QueryResponse>({
