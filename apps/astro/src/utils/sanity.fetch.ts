6db10024b137f826d4a6dab4e3787a826fca8a93
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2025-01-30 08:56:16 +0100

    get back env Token

--- apps/astro/src/utils/sanity.fetch.ts
+++ apps/astro/src/utils/sanity.fetch.ts
@@ -1,10 +1,7 @@
 import { createClient, type QueryParams } from '@sanity/client';
 import { isPreviewDeployment } from './is-preview-deployment';
 
-// const SANITY_API_TOKEN = import.meta.env.SANITY_API_TOKEN || process.env.SANITY_API_TOKEN;
-
-const SANITY_API_TOKEN =
-  'skICw2xTUWk2U5qoHiucNsJbNMxpv5AGrXIlrveughNszxIRXpP6URbe1lsmoVwp7pzPG093VpSvhwFPMgWGNJJ0Bx0aIcLErLgvI252SRh76hJHguwOvBTmMfUTkmpPcy1R5QIm7uwE6dllUWz3MP1g7ab6284MtIPTj0jo6gUm2OExO6uS';
+const SANITY_API_TOKEN = import.meta.env.SANITY_API_TOKEN || process.env.SANITY_API_TOKEN;
 
 if (isPreviewDeployment && !SANITY_API_TOKEN) {
   console.warn('\x1b[33m%s\x1b[0m', 'The `SANITY_API_TOKEN` environment variable is required.');
